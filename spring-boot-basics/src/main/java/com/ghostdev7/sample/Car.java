bc0a31367232e7f37821dd8e448af7c3967ad675
Sameer Srivastava <devx.ghost@gmail.com>
2025-10-20 00:26:25 +0530

    @Primary and @Qualifier annotations

--- spring-boot-basics/src/main/java/com/ghostdev7/sample/Car.java
+++ spring-boot-basics/src/main/java/com/ghostdev7/sample/Car.java
@@ -1,11 +1,35 @@
 package com.ghostdev7.sample;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-class Engine {
+@Primary
+class PetrolEngine implements Engine {
+
+    @Override
     public void start() {
-        System.out.println("Engine started");
+        System.out.println("Petrol engine started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Petrol engine stopped");
+    }
+}
+
+@Component
+class DieselEngine implements Engine {
+
+    @Override
+    public void start() {
+        System.out.println("Diesel engine started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Diesel engine stopped");
     }
 }
 
@@ -13,7 +37,7 @@ class Engine {
 public class Car {
     private final Engine engine;
 
-    public Car(Engine engine) {
+    public Car(@Qualifier("dieselEngine") Engine engine) {
         this.engine = engine;
     }
 
